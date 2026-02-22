90a8a608f787ed0a1c3f31d9eed2b7ba3feee0a2
masch <the.masch@gmail.com>
2026-02-22 12:46:10 -0300

    feat: Add GitHub Pages deploy action

--- dapp/vite.config.ts
+++ dapp/vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/bda-rwa-natural-resources/",
   plugins: [react()],
   envPrefix: ["VITE_", "PUBLIC_"],
 });
